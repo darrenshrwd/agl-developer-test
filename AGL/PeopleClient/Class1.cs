fe6d1d3b3ab0d1817d3d7979f5d1419e94b4f3aa
Darren Sherwood <darrenshrwd@yahoo.com>
2017-09-14 10:38:02 +1000

    Initial commit

---
+++ AGL/PeopleClient/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGL.Client
+{
+    using System.Globalization;
+    using System.Net.Http;
+
+    public class PeopleClient
+    {
+
+        private readonly HttpClient _httpClient;
+
+        private const string ServiceUri = "http://agl-developer-test.azurewebsites.net/people.json";
+        private const int TimeoutSeconds = 60;
+
+        public PeopleClient()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(ServiceUri),
+                Timeout = TimeSpan.FromSeconds(TimeoutSeconds)
+            };
+        }
+
+
+        private async Task PingOneSitemapToOneLocation(string sitemapUrl)
+        {
+            var response = await _httpClient.GetAsync(sitemapUrl);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("df");
+            }
+        }
+
+    }
+}
